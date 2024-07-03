969e144141d0e97e3b023578cecf14b9846fa86c
Daniel Lobo <153790872+Bondia23@users.noreply.github.com>
2024-07-03 15:45:50 -0300

    Add files via upload

---
+++ py3.py
@@ -0,0 +1,27 @@
+import sys
+
+while True:
+    comeco = input('Deseja: cadastrar (1), fazer login (2), desligar (3) ')
+    if comeco == '1':
+        usuario = input('Digite seu usuario: ')
+        senha = input('Digite sua senha: ')
+        print('Cadastro efetuado com sucesso! ')
+    elif comeco == '2':
+        tentativas = 3
+        while tentativas > 0:
+            usuario1 = input("Digite seu usuario: ")
+            senha1 = input('Digite sua senha: ')
+            if usuario1 == usuario and senha1 == senha:
+                print("Login confirmado")
+                sys.exit(0)  
+            else:
+                tentativas -= 1
+                print(f"Tente denovo, voce tem {tentativas} tentativas.")
+                if tentativas == 0:
+                    print("Voce atingiu o numero maximo de tentativas, o celular agora está bloqueado.")
+                    sys.exit(0) 
+    elif comeco == '3':
+        print('Desligando')
+        break  
+    else:
+        print('Opção inválida')
