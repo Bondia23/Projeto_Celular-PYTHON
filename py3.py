b23fe6fe6847db9358c912b53c10b822e39fd110
Daniel Lobo <153790872+Bondia23@users.noreply.github.com>
2024-07-03 15:58:43 -0300

    Update py3.py

--- py3.py
+++ py3.py
@@ -1,12 +1,15 @@
 import sys
-
-while True:
-    comeco = input('Deseja: cadastrar (1), fazer login (2), desligar (3) ')
-    if comeco == '1':
+inicio = input('Deseja incializar? s/n')
+if inicio == 's':
+    print('---------Bem-Vindo-------------- ')
+    print('Deseja:')
+    while True:
+      comeco = input('Deseja: cadastrar (1), fazer login (2), desligar (3) ')
+      if comeco == '1':
         usuario = input('Digite seu usuario: ')
         senha = input('Digite sua senha: ')
         print('Cadastro efetuado com sucesso! ')
-    elif comeco == '2':
+      elif comeco == '2':
         tentativas = 3
         while tentativas > 0:
             usuario1 = input("Digite seu usuario: ")
@@ -20,8 +23,12 @@ while True:
                 if tentativas == 0:
                     print("Voce atingiu o numero maximo de tentativas, o celular agora está bloqueado.")
                     sys.exit(0) 
-    elif comeco == '3':
-        print('Desligando')
-        break  
-    else:
-        print('Opção inválida')
+      elif comeco == '3':
+                 print('Desligando')
+                 break
+      else:
+        print('Opção inválida') 
+        break
+if inicio == 'n':    
+    print('Desligando')
+         
